e9c1294797e81e4b8341cb768476efd61ed6eee5
mara1esh <valerasheligan@gmail.com>
2020-01-28 15:51:38 +0200

    Updates

---
+++ precache-manifest.57e46aa6ff82be2ddb6d85e2c7e98bf7.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80e5a426944c4f7a770ab2f8b149ab4c",
+    "url": "/overview/index.html"
+  },
+  {
+    "revision": "995861b078101bb68588",
+    "url": "/overview/static/css/main.69e58ebf.chunk.css"
+  },
+  {
+    "revision": "3bcd0e795301ef20ff5d",
+    "url": "/overview/static/js/2.3e8ff126.chunk.js"
+  },
+  {
+    "revision": "eeff5171237e45569645a71691d7ac5c",
+    "url": "/overview/static/js/2.3e8ff126.chunk.js.LICENSE"
+  },
+  {
+    "revision": "995861b078101bb68588",
+    "url": "/overview/static/js/main.10f82f1d.chunk.js"
+  },
+  {
+    "revision": "1ec713498e4048c48ff4",
+    "url": "/overview/static/js/runtime-main.31e002dc.js"
+  },
+  {
+    "revision": "4ed779d2d4302da703541a088e10f9b6",
+    "url": "/overview/static/media/asana.4ed779d2.svg"
+  },
+  {
+    "revision": "b1b360f517849f9eb939165d18738714",
+    "url": "/overview/static/media/bootstrap.b1b360f5.svg"
+  },
+  {
+    "revision": "b675af6016eed403de51899be75efe59",
+    "url": "/overview/static/media/css3.b675af60.svg"
+  },
+  {
+    "revision": "b73214e3ebad909ef6891be949c39947",
+    "url": "/overview/static/media/git.b73214e3.svg"
+  },
+  {
+    "revision": "617870e422d4b75087cdcf9ff298032e",
+    "url": "/overview/static/media/github.617870e4.svg"
+  },
+  {
+    "revision": "d408f1092990152c0af2029b108e0e9e",
+    "url": "/overview/static/media/graphql.d408f109.svg"
+  },
+  {
+    "revision": "3056bf2f96c40cd0a814b355959a6983",
+    "url": "/overview/static/media/html5.3056bf2f.svg"
+  },
+  {
+    "revision": "e848a545d5372414bc134aa269cc509c",
+    "url": "/overview/static/media/inrecovery_logo.e848a545.png"
+  },
+  {
+    "revision": "765f117a6591f714cafe2986446e1686",
+    "url": "/overview/static/media/invision.765f117a.svg"
+  },
+  {
+    "revision": "1f6069572c9380a733700436d2980f8a",
+    "url": "/overview/static/media/javascript.1f606957.svg"
+  },
+  {
+    "revision": "619231ff8ec7a6b4d39aede30fa9890e",
+    "url": "/overview/static/media/linkedin.619231ff.svg"
+  },
+  {
+    "revision": "9e8d156cc78bdd2c1114b041237e466d",
+    "url": "/overview/static/media/material-ui.9e8d156c.svg"
+  },
+  {
+    "revision": "63d4455b99c2f3dc0640804ebc34d7e6",
+    "url": "/overview/static/media/me.63d4455b.jpg"
+  },
+  {
+    "revision": "72b212f22ecdd35c96d2078e5afb7c91",
+    "url": "/overview/static/media/mntu.72b212f2.jpg"
+  },
+  {
+    "revision": "8f2548d5f68478e29f805bff7694d0d9",
+    "url": "/overview/static/media/mongodb.8f2548d5.svg"
+  },
+  {
+    "revision": "27b0eaaafe77b376d8bf159a87fe36aa",
+    "url": "/overview/static/media/mysql.27b0eaaa.svg"
+  },
+  {
+    "revision": "15536a0b49fd9022ce2e89b9ded958f6",
+    "url": "/overview/static/media/protonmail.15536a0b.svg"
+  },
+  {
+    "revision": "092e0882358cd09dbe9376b64d0f2a0e",
+    "url": "/overview/static/media/react.092e0882.svg"
+  },
+  {
+    "revision": "280e514ca216947a428b9fbe0a9e183b",
+    "url": "/overview/static/media/redux.280e514c.svg"
+  },
+  {
+    "revision": "643d0db4b0852bdf249ac130d63d1fd6",
+    "url": "/overview/static/media/sass.643d0db4.svg"
+  },
+  {
+    "revision": "2f00cf6ec6dffecab543d1206e7a4712",
+    "url": "/overview/static/media/semaphoreci.2f00cf6e.svg"
+  },
+  {
+    "revision": "e033a1eb28eaa82e370016f652d71c9f",
+    "url": "/overview/static/media/tangram_logo.e033a1eb.jpg"
+  },
+  {
+    "revision": "8b0d0f2cc155066accf169750de78a93",
+    "url": "/overview/static/media/telegram.8b0d0f2c.svg"
+  },
+  {
+    "revision": "9cefeb26a8f41d85c5b1c4fc28fc6be6",
+    "url": "/overview/static/media/typescript.9cefeb26.svg"
+  },
+  {
+    "revision": "4fb1422fc808a2687aa77a9f72a4cf6f",
+    "url": "/overview/static/media/visualstudiocode.4fb1422f.svg"
+  },
+  {
+    "revision": "6478159db7c94f1220780b80d29697a2",
+    "url": "/overview/static/media/webpack.6478159d.svg"
+  }
+]);
